986013371599598fa550d046a5b259ef28a7469c
georgi.kichev <georgi_kichev@yahoo.ca>
2016-09-21 17:18:15 -0400

    initial commit: INCOMPLETEgit add .!

---
+++ app/src/main/java/com/example/a1333539/lab4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a1333539.lab4;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void googleMaps(View v)
+    {
+        Uri u = Uri.parse("geo:56.1304,106.3468?z=5");
+        showMap(u);
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
